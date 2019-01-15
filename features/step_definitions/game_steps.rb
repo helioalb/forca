97d3ae337d6a7f0bbd0cb8d624f48771cf619a5e
Helio Albano de Oliveira <helioalb@gmail.com>
2019-01-15 19:03:12 -0200

    Use aruba to step When

--- features/step_definitions/game_steps.rb
+++ features/step_definitions/game_steps.rb
@@ -1,8 +1,9 @@
 # encoding: UTF-8
 
 Quando /^começo um novo jogo$/ do
-  game = Game.new
-  game.start
+  steps %{
+    * I run `forca` interactively
+  }
 end
 
 Então /^vejo na tela:$/ do |string|
