19b2eacf9a23b19467159d8244b330632c3e8401
Helio Albano de Oliveira <helioalb@gmail.com>
2019-01-16 12:30:17 -0200

    Implement step definition

--- features/step_definitions/game_steps.rb
+++ features/step_definitions/game_steps.rb
@@ -13,3 +13,7 @@ end
 Dado /^que comecei um jogo$/ do
   start_new_game
 end
+
+Quando /^escolho que a palavra a ser sorteada deverá ter "(.*?)" letras$/ do |number_of_letters|
+  type(number_of_letters)
+end
