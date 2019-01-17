3a4318fd2e51bc571a608f8b73b3209544acd63f
Helio Albano de Oliveira <helioalb@gmail.com>
2019-01-17 14:56:31 -0200

    Refactor specs

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,18 +4,18 @@ require 'game'
 require 'spec_helper'
 
 describe Game do
+  let(:output) { double('output') }
+
+  subject(:game) { Game.new(output) }
+
   describe '#ended?' do
     it 'returns false when the game just started' do
-      game = Game.new
       expect(game).not_to be_ended
     end
   end
 
   describe '#start' do
     it 'prints the initial message' do
-      output = double('output')
-      game = Game.new(output)
-
       initial_message = 'Bem-vindo ao jogo da forca!'
 
       expect(output).to receive(:puts).with(initial_message)
@@ -24,4 +24,3 @@ describe Game do
     end
   end
 end
-
