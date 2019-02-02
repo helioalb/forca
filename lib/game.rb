f4f75d819f0515b20b24d68f345a0425f1739613
Helio Albano de Oliveira <helioalb@gmail.com>
2019-02-02 13:08:42 -0200

    Extract logic to raffle_word method

--- lib/game.rb
+++ lib/game.rb
@@ -21,15 +21,19 @@ class Game
     if player_input == 'fim'
       @ended = true
     else
-      word_length = player_input.to_i
-      words = %w(hi mom game fruit)
-
-      @raffled_word = words.detect { |word| word.length == word_length }
+      raffle_word(player_input.to_i)
     end
   end
 
   def start
     initial_message = 'Bem-vindo ao jogo da forca!'
     @ui.write(initial_message)
+  end
+
+  private
+  def raffle_word(word_length)
+    words = %w(hi mom game fruit)
+
+    @raffled_word = words.detect { |word| word.length == word_length }
   end
 end
