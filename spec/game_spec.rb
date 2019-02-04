7bdb4c52ed69b08d77b3243baee338212a9c9669
Helio Albano de Oliveira <helioalb@gmail.com>
2019-02-04 05:53:01 -0200

    Add scenario where word is greater than the permited

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -33,6 +33,19 @@ describe Game do
 
         game.next_step
       end
+
+      it "tells if it's not possible to raffle with the given length" do
+        word_length = '20'
+
+        allow(ui).to receive(:read).and_return(word_length)
+
+        error_message = "Não temos uma palavra com o tamanho desejado,\n" <<
+        "é necessário escolher outro tamanho."
+
+        expect(ui).to receive(:write).with(error_message)
+
+        game.next_step
+      end
     end
 
     context 'when the game just started' do
